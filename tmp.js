a3523133a8e262fb0e1fe35a022a1fa8c472074f
Justin Richards <richards.justind@gmail.com>
2017-02-05 23:46:25 -0600

    added some stuff to account for function not named

--- tmp.js
+++ tmp.js
@@ -1,3 +1,99 @@
+
+// Example
+
+// The purpose of this first exercise is just to show how 
+// assertions work and how the homework should be carried out. 
+// We'll do it together in class.
+
+// Part EXAMPLE
+
+// Write a function called addOne() that returns the input number, plus one. 
+
+var addOne = function(num) {
+	return num + 1
+}
+
+// Part 0
+
+// Fix the following code so doubleNum(2) wil equal 4. 
+
+var doubleNum = function(num) {
+	num * 2
+}
+
+/**
+ * PART 1
+ *
+ * Write a function called sum that takes two numbers as
+ * inputs and computes the sum of those two numbers.
+ */
+
+
+// PART 2
+
+// Write a function called average that takes two numbers
+// as inputs and returns the average of the two. To avoid
+// repeating yourself, use your sum function inside 
+// of your average function.
+
+
+// Part 4
+
+// Write a function called isNegative that will tell 
+// whether a number is negative or not.
+
+
+// Now, modify your sum function again, so that it 
+// will return null if any of the inputs is negative.
+// To avoid repeating yourself, use your isNegative 
+// function inside your sum funciton ***
+
+
+// Part 5
+
+// Write a function that will find the minimum of four 
+// input numbers. You can do it using nested if statements,
+// boolean operators, or both (but not neither).
+
+
+// Part 7
+
+// Using logical operators, write a function that will
+// return true if either input is a string, but not 
+// both or neither. 
+
+
+// Part 8
+
+// Write a function called doTwice that takes as input a 
+// function and invokes that function twice. 
+
+// For testing purposes, I've included some functions that
+// modify global variables, although that's not a good
+// pattern for production code.
+
+
+// Part 9
+
+// Write a function called conditionallyInvoke that takes a function
+// as input. It should invoke that input function only if 
+// the value of a certain global variable, called ORACLE, is 
+// "YES." Otherwise, it will does nothing.
+
+
+// Part 10
+
+// Make the following assertion work:
+	
+// Part 11
+
+// Want more?
+
+	
+// Part 12
+
+// This can't be healthy...
+
 var chai = require('chai'),
 	expect = chai.expect
 	should = chai.should
