167a11045a7195cc707f7ed502bb48c4bc060f45
Justin Richards <richards.justind@gmail.com>
2017-02-07 09:02:48 -0600

    simplified describe syntax

--- test.js
+++ test.js
@@ -18,64 +18,62 @@ function checkFuncBasics(name,argNum) {
 	if (argNum) assert.equal(func.length,argNum,'Your function should take ' + argNum + ' argument(s) (that means inputs). Does it?')
 }
 
-describe('Function', function() {
-	describe('#sumOfArray()', function(){
-		it('should calculate sum of all numbers in an array', function(){
-			checkFuncBasics('sumOfArray',1)
-			assert.equal(3, sumOfArray([1, 2]))
-			assert.equal(0, sumOfArray([]))
-			assert.equal(6, sumOfArray([1, 2, 3]))
-			assert.equal(27, sumOfArray([10, 9, 8]))
-		})
+describe('sumOfArray()', function(){
+	it('should calculate sum of all numbers in an array', function(){
+		checkFuncBasics('sumOfArray',1)
+		assert.equal(3, sumOfArray([1, 2]))
+		assert.equal(0, sumOfArray([]))
+		assert.equal(6, sumOfArray([1, 2, 3]))
+		assert.equal(27, sumOfArray([10, 9, 8]))
 	})
-	describe('#maxOfArray()', function(){
-		it('should return the highest number of an array', function(){
-			checkFuncBasics('maxOfArray',1)
-			assert.equal(4, maxOfArray([2, 4, 3]))
-			assert.equal(100, maxOfArray([10,9,8,100,7,6]))
-		})
+})
+describe('maxOfArray()', function(){
+	it('should return the highest number of an array', function(){
+		checkFuncBasics('maxOfArray',1)
+		assert.equal(4, maxOfArray([2, 4, 3]))
+		assert.equal(100, maxOfArray([10,9,8,100,7,6]))
 	})
-	describe('#isVowel()', function(){
-		it('should return true if character is a vowel, otherwise false', function(){
-			checkFuncBasics('isVowel',1)
-			assert.equal(false, isVowel(0))
-			assert.equal(false, isVowel("B"))
-			assert.equal(false, isVowel("b"))
-			assert.equal(true, isVowel("a"))
-			assert.equal(true, isVowel("E"))
-		})
+})
+describe('isVowel()', function(){
+	it('should return true if character is a vowel, otherwise false', function(){
+		checkFuncBasics('isVowel',1)
+		assert.equal(false, isVowel(0))
+		assert.equal(false, isVowel("B"))
+		assert.equal(false, isVowel("b"))
+		assert.equal(true, isVowel("a"))
+		assert.equal(true, isVowel("E"))
 	})
-	describe('#reverse()', function(){
-		it('should return the reversal of an input string', function(){
-			checkFuncBasics('reverse',1)
-			assert.equal("skoob", reverse("books"))
-			assert.equal("elbuort on tnaw t'nod ew", reverse("we don't want no trouble"))
-		})
+})
+describe('reverse()', function(){
+	it('should return the reversal of an input string', function(){
+		checkFuncBasics('reverse',1)
+		assert.equal("skoob", reverse("books"))
+		assert.equal("elbuort on tnaw t'nod ew", reverse("we don't want no trouble"))
 	})
-	describe('#fizzbuzz()', function(){
-		it('should meet the standards listed in Part 4 instructions', function(){
-			checkFuncBasics('fizzbuzz',2)
-			assert.equal(".", fizzbuzz(1))
-			assert.equal("..", fizzbuzz(2))
-			assert.equal("..fizz", fizzbuzz(3))
-			assert.equal("..fizz.buzz", fizzbuzz(5))
-			assert.equal("..fizz.buzzfizz..fizzbuzz", fizzbuzz(10))
-		})
+})
+describe('fizzbuzz()', function(){
+	it('should meet the standards listed in Part 4 instructions', function(){
+		checkFuncBasics('fizzbuzz',2)
+		assert.equal(".", fizzbuzz(1))
+		assert.equal("..", fizzbuzz(2))
+		assert.equal("..fizz", fizzbuzz(3))
+		assert.equal("..fizz.buzz", fizzbuzz(5))
+		assert.equal("..fizz.buzzfizz..fizzbuzz", fizzbuzz(10))
 	})
-	describe('#findLongestWord', function(){
-		it('should return the longest word in a string of words', function(){
-			checkFuncBasics('findLongestWord',1)
-			assert.equal("book", findLongestWord("a book full of dogs"))
-			assert.equal("Texas", findLongestWord("don't mess with Texas"))
-		})
+})
+describe('findLongestWord()', function(){
+	it('should return the longest word in a string of words', function(){
+		checkFuncBasics('findLongestWord',1)
+		assert.equal("book", findLongestWord("a book full of dogs"))
+		assert.equal("Texas", findLongestWord("don't mess with Texas"))
 	})
-	describe('#GCD', function(){
-		it('should find the Greatest Common Denominator of two numbers', function(){
-			checkFuncBasics('GCD',2)
-			assert.equal(1, GCD(5,1))
-			assert.equal(3, GCD(15,3))
-			assert.equal(5, GCD(15,5))
-			assert.equal(10, GCD(50,20))
-		})
+})
+describe('GCD()', function(){
+	it('should find the Greatest Common Denominator of two numbers', function(){
+		checkFuncBasics('GCD',2)
+		assert.equal(1, GCD(5,1))
+		assert.equal(3, GCD(15,3))
+		assert.equal(5, GCD(15,5))
+		assert.equal(10, GCD(50,20))
 	})
 })
--- tmp.js
+++ tmp.js
@@ -1,134 +1,64 @@
 var assert = require('chai').assert
+/**
+ * PART 0
+ *
+ * Write a function that calculates the sum of all the numbers in an array
+ */
 
-// Example
+// PART 1
 
-// The purpose of this first exercise is just to show how 
-// assertions work and how the homework should be carried out. 
-// We'll do it together in class.
+// Write a function maxOfArray() that takes an array of
+// numbers as an argument and returns the highest number in the array.
 
-// Part EXAMPLE
-
-// Write a function called addOne() that returns the input number, plus one. 
-
-var addOne = function(num) {
-	return num + 1
-}
-
-// Part 1
-
-// Fix the following code so doubleNum(2) wil equal 4. 
-
-var doubleNum = function(num) {
-	num * 2
-}
 
 /**
  * PART 2
  *
- * Write a function called sum that takes two numbers as
- * inputs and computes the sum of those two numbers.
+ * Write a function isVowel() that takes a character (i.e. a string of length 1)
+ * as input and returns true if it is a vowel, false otherwise.
  */
 
 
-// PART 3
-
-// Write a function called average that takes two numbers
-// as inputs and returns the average of the two. To avoid
-// repeating yourself, use your sum function inside 
-// of your average function.
-
-
-// Part 4
-
-// Write a function called isNegative that will tell 
-// whether a number is negative or not.
+/**
+ * Part 3
+ *
+ * Define a function reverse() that computes
+ * the reversal of a string. For example,
+ * reverse("skoob") should return the
+ * string "books".
+ */
 
 
-// Now, modify your sum function again, so that it 
-// will return null if any of the inputs is negative.
-// To avoid repeating yourself, use your isNegative 
-// function inside your sum funciton ***
+/**
+ * Part 4
+ *
+ * write a function the returns a fizzbuzz string for an input number. 
+ A fizzbuzz string is defined as the following:
+ For every number from 1 to the input number...
+ * - if that number isn't a multiple of 3 or 5, add a period "." to the fizzbuzz string
+ * - for every number that is a multiple of 3 (but not 5), add "fizz" to the fizzbuzz string
+ * - for every number that is a multiple of 5 (but not 3), add "buzz" to the fizzbuzz string
+ * - for every number that is a multiple of 3 and 5, add "fizzbuzz" to the fizzbuzz string
+ For example, the fizzbuzz string for the number 3 is "..fizz"
+ For the number 15, the fizzbuzz string is "..fizz.buzzfizz..fizzbuzz.fizz..fizzbuzz"
+ */
 
 
-// Part 5
-
-// Write a function that will find the minimum of four 
-// input numbers. You can do it using nested if statements,
-// boolean operators, or both (but not neither).
-
-
-// Part 6
-
-// Using logical operators, write a function that will
-// return true if either input is a string, but not 
-// both or neither. 
+/**
+ * Part 5
+ *
+ * Write a function findLongestWord() that takes a string of 
+ words and returns the longest word.
+ * i.e. findLongestWord("a book full of dogs") should return "book"
+ */
 
 
-// HARD MODE
-
-// Part 7
-
-// Write a function called doTwice that takes as input a 
-// function and invokes that function twice. 
-
-// For testing purposes, I've included some functions that
-// modify global variables, although that's not a good
-// pattern for production code.
-
-var doTwice = function() {
-
-}
-
-var helloWorld = function() {
-	console.log('hello world')
-}
-
-
-doTwice(helloWorld) // should print 'hello world' to the console twice. 
-
-
-// Part 8
-
-// Write a function called conditionallyInvoke that takes a function
-// as input. It should invoke that input function only if 
-// the value of a certain global variable, called ORACLE, is 
-// "YES." Otherwise, it will does nothing.
-
-var conditionallyInvoke = function() {
-
-}
-
-var ORACLE = 'NO'
-
-conditionallyInvoke(helloWorld) // should do nothing
-
-var ORACLE = 'YES'
-
-conditionallyInvoke(helloWorld) // should print hello world
-
-
-// EPIC MODE
-
-// For parts 9 through 11, your only instructions are to make the tests pass.
-
-// Part 9
-
-var factory = function() {
-
-}
-
-
-// Part 10
-
-var factory2 = function() {
-	// you want more?
-}
-	
-// Part 11
-
-var factory3 = function(stuff) {
-	// This can't be healthy...	
-}
+/**
+ * PART 6
+ *
+ * write a function that returns the Greatest Common Denominator of two numbers
+ * - if no GCD exists, return 1
+ */
 
 console.log('********************************************************************************************************************************************')
 console.log('')
@@ -140,10 +70,6 @@ var chai = require('chai'),
 	should = chai.should
 	assert = chai.assert
 
-var NUMBER,
-	ORACLE
-
-
 function checkFuncBasics(name,argNum) {
 	try {
 		var func = eval(name)
@@ -154,100 +80,62 @@ function checkFuncBasics(name,argNum) {
 	if (argNum) assert.equal(func.length,argNum,'Your function should take ' + argNum + ' argument(s) (that means inputs). Does it?')
 }
 
-describe('Function', function() {
-	describe('#doubleNum()', function(){
-		it('should return a doubled input', function(){
-			checkFuncBasics('doubleNum',1)
-			assert.equal(10, doubleNum(5))
-		})
+describe('sumOfArray()', function(){
+	it('should calculate sum of all numbers in an array', function(){
+		checkFuncBasics('sumOfArray',1)
+		assert.equal(3, sumOfArray([1, 2]))
+		assert.equal(0, sumOfArray([]))
+		assert.equal(6, sumOfArray([1, 2, 3]))
+		assert.equal(27, sumOfArray([10, 9, 8]))
 	})
-	describe('#sum()', function(){
-		it('should return the sum of two inputs', function(){
-			checkFuncBasics('sum',2)
-			assert.equal(19, sum(8, 11))
-			assert.equal(104, sum(4, 100))
-			assert.equal(6176, sum(867, 5309))
-			assert.equal(null, sum('867', 5309))
-			assert.equal(null, sum(true, 5))
-			assert.equal(null, sum(5,-5))
-		})
+})
+describe('maxOfArray()', function(){
+	it('should return the highest number of an array', function(){
+		checkFuncBasics('maxOfArray',1)
+		assert.equal(4, maxOfArray([2, 4, 3]))
+		assert.equal(100, maxOfArray([10,9,8,100,7,6]))
 	})
-	describe('#average()', function(){
-		it('should return the average of two inputs', function(){
-			checkFuncBasics('average',2)
-			assert.equal(10, average(8, 12))
-			assert.equal(150, average(100, 200))
-		})
+})
+describe('isVowel()', function(){
+	it('should return true if character is a vowel, otherwise false', function(){
+		checkFuncBasics('isVowel',1)
+		assert.equal(false, isVowel(0))
+		assert.equal(false, isVowel("B"))
+		assert.equal(false, isVowel("b"))
+		assert.equal(true, isVowel("a"))
+		assert.equal(true, isVowel("E"))
 	})
-	describe('#isNegative', function(){
-		it('should tell whether a number is negative or not', function(){
-			checkFuncBasics('isNegative',1)
-			assert.equal(false, isNegative(10))
-			assert.equal(false, isNegative(0))
-			assert.equal(true, isNegative(-999))
-		})
+})
+describe('reverse()', function(){
+	it('should return the reversal of an input string', function(){
+		checkFuncBasics('reverse',1)
+		assert.equal("skoob", reverse("books"))
+		assert.equal("elbuort on tnaw t'nod ew", reverse("we don't want no trouble"))
 	})
-	describe('#minimum', function(){
-		it('should find the minimum of four input numbers', function(){
-			checkFuncBasics('minimum',4)
-			assert.equal(0, minimum(1,2,4,0))
-			assert.equal(-99, minimum(1000,-2,-99,50))
-			assert.equal(-1000, minimum(-1000,-2,-99,50))
-			assert.equal(-2, minimum(1000,-2,99,50))
-		})
+})
+describe('fizzbuzz()', function(){
+	it('should meet the standards listed in Part 4 instructions', function(){
+		checkFuncBasics('fizzbuzz',2)
+		assert.equal(".", fizzbuzz(1))
+		assert.equal("..", fizzbuzz(2))
+		assert.equal("..fizz", fizzbuzz(3))
+		assert.equal("..fizz.buzz", fizzbuzz(5))
+		assert.equal("..fizz.buzzfizz..fizzbuzz", fizzbuzz(10))
 	})
-	describe('#justOneString', function(){
-		it('should return true if one input is a string', function(){
-			checkFuncBasics('justOneString',2)
-			assert.equal(true, justOneString('a',5))
-			assert.equal(true, justOneString(6,'dotron'))
-			assert.equal(false, justOneString('peanut','butter'))
-			assert.equal(false, justOneString(8,null))
-		})
+})
+describe('findLongestWord()', function(){
+	it('should return the longest word in a string of words', function(){
+		checkFuncBasics('findLongestWord',1)
+		assert.equal("book", findLongestWord("a book full of dogs"))
+		assert.equal("Texas", findLongestWord("don't mess with Texas"))
 	})
-	describe('#doTwice', function(){
-		it('should take a function as input and invokes the function twice', function(){
-			checkFuncBasics('doTwice',1)
-			NUMBER = 12
-			doTwice(function(){NUMBER += 1})
-			assert.equal(NUMBER,14)
-		})
-	})
-	describe('#conditionallyInvoke', function(){
-		it('should invoke the input function if a global variable called ORACLE stores the string "YES"', function(){
-			checkFuncBasics('conditionallyInvoke',1)
-			NUMBER = 10
-			ORACLE = 'NO'
-			conditionallyInvoke(function(){NUMBER += 1})
-			assert.equal(NUMBER,10)
-			ORACLE = 'YES'
-			conditionallyInvoke(function(){NUMBER += 1})
-			assert.equal(NUMBER,11)
-
-		})
-	})
-	describe('#factory', function(){
-		it('should work as described below', function(){
-			checkFuncBasics('factory')
-			var str = 'factory()() should equal the string "hello world"'
-			assert.typeOf(factory2(),'function',str)
-			assert.equal('hello world', factory()(), str)
-		})
-	})
-	describe('#factory2', function(){
-		it('should work as described below', function(){
-			var str = 'factory2()("you sly dog") should equal the string "you sly dog"'
-			assert.typeOf(factory2(),'function',str)
-			assert.equal('you sly dog', factory2()('you sly dog'), str)
-			assert.equal(999, factory2()(999))
-		})
-	})
-	describe('#factory3', function(){
-		it('should work as described below', function(){
-			var str = 'factory3("i have reached function nirvana")() should equal the string "i have reached function nirvana"'
-			assert.typeOf(factory3(),'function',str)
-			assert.equal('i have reached function nirvana', factory3('i have reached function nirvana')(), str)
-			assert.equal(100, factory3(100)())
-		})
+})
+describe('GCD()', function(){
+	it('should find the Greatest Common Denominator of two numbers', function(){
+		checkFuncBasics('GCD',2)
+		assert.equal(1, GCD(5,1))
+		assert.equal(3, GCD(15,3))
+		assert.equal(5, GCD(15,5))
+		assert.equal(10, GCD(50,20))
 	})
 })
